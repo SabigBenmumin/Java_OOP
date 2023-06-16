2d1fc7de6a074b324d47702f2b4902821632e4c6
SabigBenmumin <totpophy14755@gmail.com>
2023-06-17 00:41:06 +0700

    add new file for chapter 2 last commit 16/6/66

---
+++ Chapter2/Sating2.java
@@ -0,0 +1,6 @@
+public class Sating2 {
+    public static void main(String[] args) {
+        String name = "Sabig";
+        System.out.printf("The length of %s is %s",name,name.length());
+    }
+}
---
+++ Chapter2/SatingB2L.java
@@ -0,0 +1,9 @@
+public class SatingB2L {
+    public static void main(String[] args) {
+        String Originalname,Bigname,Lowername;
+        Originalname = "Sabig";
+        Bigname = Originalname.toUpperCase();
+        Lowername = Bigname.toLowerCase();
+        System.out.printf("upper name is %s\nlower name is %s",Bigname,Lowername);
+    }
+}

ea57387d020bc4b2aca72ec928760b66022c9fdf
SabigBenmumin <totpophy14755@gmail.com>
2023-07-20 14:04:32 +0700

    add chapter 3

---
+++ Chapter3/ex_1.java
@@ -0,0 +1,15 @@
+public class ex_1 {
+    public static void main(String[] args) {
+        // relational operators
+        int heigher,lower;
+        heigher = 10;
+        lower = 2;
+        System.out.println(heigher > lower); // more than
+        System.out.println(heigher < lower); // less than
+        System.out.println(heigher >= heigher); // more than or equal to
+        System.out.println(heigher <= lower); // less than or equal to
+        System.out.println(heigher == lower); // equal
+        System.out.println(heigher != lower); // none equal
+        System.out.println(heigher << 1); // << will slide bit to right
+    }
+}
---
+++ Chapter3/ex_2.java
@@ -0,0 +1,28 @@
+public class ex_2 {
+    public static void main(String[] args) {
+        // logical operators
+        // and
+        System.out.println("AND");
+        System.out.println(true && false);
+        System.out.println(true && true);
+        System.out.println("");
+        // or
+        System.out.println("OR");
+        System.out.println(true || true);
+        System.out.println(true || false);
+        System.out.println(false || true);
+        System.out.println(false || false);
+        System.out.println("");
+        // not
+        System.out.println("NOT");
+        System.out.println(!true);
+        System.out.println(!false);
+        System.out.println("");
+        // exclusive-Or
+        System.out.println("XOR");
+        System.out.println(true ^ true);
+        System.out.println(true ^ false);
+        System.out.println(false ^ true);
+        System.out.println(false ^ false);
+    }
+}
